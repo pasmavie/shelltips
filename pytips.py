a29a194830ec138a66577532302a76ac5b9c11b4
gio <[omitted]>
2021-02-09 19:15:54 +0100

    .

--- pytips.py
+++ pytips.py
@@ -1,31 +1,7 @@
-#
-# Matplotlib config stolen here and there
-#
+# attach REPL to an active jupyter kernel
+jupyter console --active
 
-import matplotlib as mpl
-from cycler import cycler
-
-mpl.rcParams.update(
-    {
-        "figure.figsize": (12.5, 4),
-        "lines.linewidth": 2.0,
-        "axes.edgecolor": "#bcbcbc",
-        "patch.linewidth": 0.5,
-        "legend.fancybox": True,
-        "axes.prop_cycle": cycler('color', [
-            "#348ABD",
-            "#A60628",
-            "#7A68A6",
-            "#467821",
-            "#CF4457",
-            "#188487",
-            "#E24A33"
-        ]),
-        "axes.facecolor": "#eeeeee",
-        "axes.labelsize": "large",
-        "axes.grid": True,
-        "patch.edgecolor": "#eeeeee",
-        "axes.titlesize": "x-large",
-        "svg.fonttype": "path"
-    }
-)
+# Display all columns in a pandas df
+import pandas as pd
+from IPython.display import display
+pd.options.display.max_columns = None
